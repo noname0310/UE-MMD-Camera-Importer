a4ed7267d7892552c38a9be646069bccf9d43fc3
noname <48761044+noname0310@users.noreply.github.com>
2023-01-17 22:12:51 +0900

    implement vmd parsing

--- Plugins/MMDCameraImporter/Source/MMDCameraImporter/Private/VMDImporter.cpp
+++ Plugins/MMDCameraImporter/Source/MMDCameraImporter/Private/VMDImporter.cpp
@@ -34,6 +34,7 @@ bool FVmdImporter::IsValidVmdFile()
 		return false;
 	}
 
+	FileReader->Seek(0);
 	uint8 Magic[30];
 	FileReader->Serialize(Magic, sizeof Magic);
 	if (FMmdImportHelper::ShiftJisToFString(Magic, sizeof Magic) != "Vocaloid Motion Data 0002")
@@ -141,7 +142,7 @@ FVmdParseResult FVmdImporter::ParseVmdFile()
 
 		if (!FileReader.IsValid())
 		{
-			UE_LOG(LogTemp, Error, TEXT("Failed to open file: %s"), *FilePath);
+			UE_LOG(LogMMDCameraImporter, Error, TEXT("Can't open file(%s)"), *FilePath);
 
 			FVmdParseResult FailedResult;
 			FailedResult.bIsSuccess = false;
@@ -149,14 +150,110 @@ FVmdParseResult FVmdImporter::ParseVmdFile()
 			return FailedResult;
 		}
 	}
+    
+    FScopedSlowTask ImportVmdTask(7, LOCTEXT("ReadingVMDFile", "Reading VMD File"));
+    ImportVmdTask.MakeDialog(true, true);
 
-	FScopedSlowTask ImportVmdTask(20, LOCTEXT("OpeningFile", "Reading File"));
-	ImportVmdTask.MakeDialog(false, true);
+    FVmdParseResult VmdParseResult;
+    VmdParseResult.bIsSuccess = false;
+    
+    if (ImportVmdTask.ShouldCancel())
+    {
+        return VmdParseResult;
+    }
+    ImportVmdTask.EnterProgressFrame(1, LOCTEXT("ReadingVMDFileHeader", "Reading Header"));
+    FileReader->Seek(0);
+    FileReader->Serialize(&VmdParseResult.Header, sizeof(FVmdObject::FHeader));
 
-	return {};
+    if (ImportVmdTask.ShouldCancel())
+    {
+        return VmdParseResult;
+    }
+    ImportVmdTask.EnterProgressFrame(1, LOCTEXT("ReadingVMDFileBoneKeyFrames", "Reading Bone Key Frames"));
+    uint32 BoneKeyFrameCount = 0;
+    FileReader->Serialize(&BoneKeyFrameCount, sizeof(uint32));
+    VmdParseResult.BoneKeyFrames.SetNum(BoneKeyFrameCount);
+    FileReader->Serialize(VmdParseResult.BoneKeyFrames.GetData(), sizeof(FVmdObject::FBoneKeyFrame) * BoneKeyFrameCount);
+
+    if (ImportVmdTask.ShouldCancel())
+    {
+        return VmdParseResult;
+    }
+    ImportVmdTask.EnterProgressFrame(1, LOCTEXT("ReadingVMDFileMorphKeyFrames", "Reading Morph Key Frames"));
+    uint32 MorphKeyFrameCount = 0;
+    FileReader->Serialize(&MorphKeyFrameCount, sizeof(uint32));
+    VmdParseResult.MorphKeyFrames.SetNum(MorphKeyFrameCount);
+    FileReader->Serialize(VmdParseResult.MorphKeyFrames.GetData(), sizeof(FVmdObject::FMorphKeyFrame) * MorphKeyFrameCount);
+
+    if (ImportVmdTask.ShouldCancel())
+    {
+        return VmdParseResult;
+    }
+    ImportVmdTask.EnterProgressFrame(1, LOCTEXT("ReadingVMDFileCameraKeyFrames", "Reading Camera Key Frames"));
+    uint32 CameraKeyFrameCount = 0;
+	FileReader->Serialize(&CameraKeyFrameCount, sizeof(uint32));
+    VmdParseResult.CameraKeyFrames.SetNum(CameraKeyFrameCount);
+    FileReader->Serialize(VmdParseResult.CameraKeyFrames.GetData(), sizeof(FVmdObject::FCameraKeyFrame) * CameraKeyFrameCount);
+
+    if (ImportVmdTask.ShouldCancel())
+    {
+        return VmdParseResult;
+    }
+    ImportVmdTask.EnterProgressFrame(1, LOCTEXT("ReadingVMDFileLightKeyFrames", "Reading Light Key Frames"));
+    uint32 LightKeyFrameCount = 0;
+    FileReader->Serialize(&LightKeyFrameCount, sizeof(uint32));
+    VmdParseResult.LightKeyFrames.SetNum(LightKeyFrameCount);
+    FileReader->Serialize(VmdParseResult.LightKeyFrames.GetData(), sizeof(FVmdObject::FLightKeyFrame) * LightKeyFrameCount);
+
+    if (ImportVmdTask.ShouldCancel())
+    {
+        return VmdParseResult;
+    }
+    ImportVmdTask.EnterProgressFrame(1, LOCTEXT("ReadingVMDFileSelfShadowKeyFrames", "Reading Self Shadow Key Frames"));
+    uint32 SelfShadowKeyFrameCount = 0;
+    FileReader->Serialize(&SelfShadowKeyFrameCount, sizeof(uint32));
+    VmdParseResult.SelfShadowKeyFrames.SetNum(SelfShadowKeyFrameCount);
+    FileReader->Serialize(VmdParseResult.SelfShadowKeyFrames.GetData(), sizeof(FVmdObject::FSelfShadowKeyFrame) * SelfShadowKeyFrameCount);
+
+    if (ImportVmdTask.ShouldCancel())
+    {
+        return VmdParseResult;
+    }
+    ImportVmdTask.EnterProgressFrame(1, LOCTEXT("ReadingVMDFilePropertyKeyFrames", "Reading Property Key Frames"));
+    uint32 PropertyKeyFrameCount = 0;
+    FileReader->Serialize(&PropertyKeyFrameCount, sizeof(uint32));
+    VmdParseResult.PropertyKeyFrames.SetNum(PropertyKeyFrameCount);
+    {
+        FScopedSlowTask ImportPropertyKeyFramesTask(PropertyKeyFrameCount, LOCTEXT("ReadingVMDFilePropertyKeyFrames", "Reading Property Key Frames"));
+
+        for (PTRINT i = 0; i < PropertyKeyFrameCount; ++i)
+        {
+            if (ImportPropertyKeyFramesTask.ShouldCancel())
+            {
+                return VmdParseResult;
+            }
+            ImportPropertyKeyFramesTask.EnterProgressFrame();
+
+            FVmdObject::FPropertyKeyFrame PropertyKeyFrame;
+            FileReader->Serialize(&PropertyKeyFrame, sizeof(FVmdObject::FPropertyKeyFrame));
+            VmdParseResult.PropertyKeyFrames[i].FrameNumber = PropertyKeyFrame.FrameNumber;
+            VmdParseResult.PropertyKeyFrames[i].Visible = static_cast<bool>(PropertyKeyFrame.Visible);
+
+            uint32 IkStateCount = 0;
+            FileReader->Serialize(&IkStateCount, sizeof(uint32));
+            VmdParseResult.PropertyKeyFrames[i].IkStates.SetNum(IkStateCount);
+            FileReader->Serialize(VmdParseResult.PropertyKeyFrames[i].IkStates.GetData(), sizeof(FVmdObject::FPropertyKeyFrame::FIkState) * IkStateCount);
+        }
+    }
+
+    VmdParseResult.bIsSuccess = true;
+
+    return VmdParseResult;
 }
 
 FArchive* FVmdImporter::OpenFile(const FString FilePath)
 {
 	return IFileManager::Get().CreateFileReader(*FilePath);
 }
+
+#undef LOCTEXT_NAMESPACE

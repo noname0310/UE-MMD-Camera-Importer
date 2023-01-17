5720c8e5a8c69f89ece2a7634df9a8a438bb80e9
noname <48761044+noname0310@users.noreply.github.com>
2023-01-18 00:29:57 +0900

    upload icon

--- Plugins/MMDCameraImporter/Source/MMDCameraImporter/Private/MMDCameraImporterStyle.cpp
+++ Plugins/MMDCameraImporter/Source/MMDCameraImporter/Private/MMDCameraImporterStyle.cpp
@@ -14,48 +14,48 @@ TSharedPtr<FSlateStyleSet> FMmdCameraImporterStyle::StyleInstance = nullptr;
 
 void FMmdCameraImporterStyle::Initialize()
 {
-    if (!StyleInstance.IsValid())
-    {
-        StyleInstance = Create();
-        FSlateStyleRegistry::RegisterSlateStyle(*StyleInstance);
-    }
+	if (!StyleInstance.IsValid())
+	{
+		StyleInstance = Create();
+		FSlateStyleRegistry::RegisterSlateStyle(*StyleInstance);
+	}
 }
 
 void FMmdCameraImporterStyle::Shutdown()
 {
-    FSlateStyleRegistry::UnRegisterSlateStyle(*StyleInstance);
-    ensure(StyleInstance.IsUnique());
-    StyleInstance.Reset();
+	FSlateStyleRegistry::UnRegisterSlateStyle(*StyleInstance);
+	ensure(StyleInstance.IsUnique());
+	StyleInstance.Reset();
 }
 
 FName FMmdCameraImporterStyle::GetStyleSetName()
 {
-    static FName StyleSetName(TEXT("MMDCameraImporterStyle"));
-    return StyleSetName;
+	static FName StyleSetName(TEXT("MMDCameraImporterStyle"));
+	return StyleSetName;
 }
 
 
 TSharedRef<FSlateStyleSet> FMmdCameraImporterStyle::Create()
 {
-    const FVector2D Icon20X20(20.0f, 20.0f);
+	const FVector2D Icon20X20(20.0f, 20.0f);
 
-    TSharedRef< FSlateStyleSet > Style = MakeShareable(new FSlateStyleSet("MMDCameraImporterStyle"));
-    Style->SetContentRoot(IPluginManager::Get().FindPlugin("MMDCameraImporter")->GetBaseDir() / TEXT("Resources"));
+	TSharedRef< FSlateStyleSet > Style = MakeShareable(new FSlateStyleSet("MMDCameraImporterStyle"));
+	Style->SetContentRoot(IPluginManager::Get().FindPlugin("MMDCameraImporter")->GetBaseDir() / TEXT("Resources"));
 
-    Style->Set("MMDCameraImporter.ImportVmd", new IMAGE_BRUSH_SVG(TEXT("PlaceholderButtonIcon"), Icon20X20));
+	Style->Set("MMDCameraImporter.ImportVmd", new IMAGE_BRUSH_SVG(TEXT("ImportVmd"), Icon20X20));
 
-    return Style;
+	return Style;
 }
 
 void FMmdCameraImporterStyle::ReloadTextures()
 {
-    if (FSlateApplication::IsInitialized())
-    {
-        FSlateApplication::Get().GetRenderer()->ReloadTextureResources();
-    }
+	if (FSlateApplication::IsInitialized())
+	{
+		FSlateApplication::Get().GetRenderer()->ReloadTextureResources();
+	}
 }
 
 const ISlateStyle& FMmdCameraImporterStyle::Get()
 {
-    return *StyleInstance;
+	return *StyleInstance;
 }

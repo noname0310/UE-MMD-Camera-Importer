8ecb9d2a480f436788eecb17f242480defa74d54
noname <48761044+noname0310@users.noreply.github.com>
2023-01-17 11:01:08 +0900

    create shift jis converter

---
+++ Plugins/MMDCameraImporter/Source/MMDCameraImporter/Private/MMDImportHelper.cpp
@@ -0,0 +1,50 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MMDImportHelper.h"
+
+#include "ShiftJisConvertTable.h"
+
+FString FMmdImportHelper::ShiftJisToFString(const uint8* InBuffer, const int32 InSize)
+{
+    FString Output;
+    Output.Reserve(3 * InSize); //ShiftJis won't give 4byte UTF8, so max. 3 byte per input char are needed
+    
+    PTRINT IndexInput = 0;
+
+    while (IndexInput < InSize)
+    {
+        const uint8 ArraySection = InBuffer[IndexInput] >> 4;
+
+        PTRINT ArrayOffset;
+        if (ArraySection == 0x8) ArrayOffset = 0x100; //these are two-byte shiftjis
+        else if (ArraySection == 0x9) ArrayOffset = 0x1100;
+        else if (ArraySection == 0xE) ArrayOffset = 0x2100;
+        else ArrayOffset = 0; //this is one byte shiftjis
+
+        //determining real array offset
+        if (ArrayOffset)
+        {
+            ArrayOffset += (InBuffer[IndexInput] & 0xf) << 8;
+            IndexInput += 1;
+            if (IndexInput >= InSize) break;
+        }
+        ArrayOffset += InBuffer[IndexInput];
+        IndexInput += 1;
+        ArrayOffset <<= 1;
+        
+        const uint16 UnicodeValue = (GShiftJisConvertTable[ArrayOffset] << 8) | GShiftJisConvertTable[ArrayOffset + 1];
+        Output.AppendChar(UnicodeValue);
+    }
+
+    Output.Shrink();
+    return Output;
+}
+
+FVector3f FMmdImportHelper::ConvertVectorFromMmdToUe(const FVector3f InVector)
+{
+    FVector3f Result;
+    Result.X = InVector.X * 1;
+    Result.Y = InVector.Z * -1;
+    Result.Z = InVector.Y * 1;
+    return Result;
+}
---
+++ Plugins/MMDCameraImporter/Source/MMDCameraImporter/Public/MMDImportHelper.h
@@ -0,0 +1,12 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class FMmdImportHelper
+{
+public:
+    static FString ShiftJisToFString(const uint8* InBuffer, int32 InSize);
+    static FVector3f ConvertVectorFromMmdToUe(FVector3f InVector);
+};

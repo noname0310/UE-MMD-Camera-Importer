d285058a720675c53a9733bf5ad8b162bafb403f
noname <48761044+noname0310@users.noreply.github.com>
2023-01-17 11:01:21 +0900

    FileReader Test

--- Plugins/MMDCameraImporter/Source/MMDCameraImporter/Private/VMDImporter.cpp
+++ Plugins/MMDCameraImporter/Source/MMDCameraImporter/Private/VMDImporter.cpp
@@ -2,6 +2,7 @@
 
 #include "VMDImporter.h"
 
+#include "MMDImportHelper.h"
 #include "Misc/ScopedSlowTask.h"
 
 #define LOCTEXT_NAMESPACE "FMmdCameraImporterModule"
@@ -23,7 +24,24 @@ bool FVmdImporter::IsValidVmdFile()
 		}
 	}
 
-	FileReader->
+    // ReSharper disable once CppTooWideScopeInitStatement
+    const int64 FileSize = FileReader->TotalSize();
+
+	if (FileSize < sizeof(FVmdObject::FHeader))
+	{
+		return false;
+	}
+
+    const TUniquePtr<uint8[]> Magic = TUniquePtr<uint8[]>(new uint8[30]);
+
+	FileReader->Serialize(Magic.Get(), 30);
+
+    const TUniquePtr<uint8[]> ModelName = TUniquePtr<uint8[]>(new uint8[20]);
+
+	FileReader->Serialize(ModelName.Get(), 20);
+
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *FMmdImportHelper::ShiftJisToFString(Magic.Get(), 30));
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *FMmdImportHelper::ShiftJisToFString(ModelName.Get(), 20));
 
 	return true;
 }

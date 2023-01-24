9363134b446ca088a46432e4e6489aaa4f68b00f
noname <48761044+noname0310@users.noreply.github.com>
2023-01-24 19:33:58 +0900

    add rad 2 deg

--- Plugins/MMDCameraImporter/Source/MMDCameraImporter/Private/VMDImporter.cpp
+++ Plugins/MMDCameraImporter/Source/MMDCameraImporter/Private/VMDImporter.cpp
@@ -819,7 +819,7 @@ bool FVmdImporter::ImportVmdCameraCenterTransform(
 			},
 			[UniformScale](const double Value)
 			{
-				return Value;
+				return FMath::RadiansToDegrees(Value);
 			});
 
 		ImportCameraSingleChannel<double, FMovieSceneDoubleChannel, FMovieSceneDoubleValue>(
@@ -835,7 +835,7 @@ bool FVmdImporter::ImportVmdCameraCenterTransform(
 			},
 			[UniformScale](const double Value)
 			{
-				return Value;
+				return FMath::RadiansToDegrees(Value);
 			});
 
 		ImportCameraSingleChannel<double, FMovieSceneDoubleChannel, FMovieSceneDoubleValue>(
@@ -851,7 +851,7 @@ bool FVmdImporter::ImportVmdCameraCenterTransform(
 			},
 			[UniformScale](const double Value)
 			{
-				return Value;
+				return FMath::RadiansToDegrees(Value);
 			});
 	}
 
